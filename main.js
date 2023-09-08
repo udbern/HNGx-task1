6fb7e99ba46a6e7f815dd3837dc7fc87622635ff
udbern <edwinuduakabasi@gmail.com>
2023-09-08 08:36:13 +0100

    made few adjustments

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-function updateDateTime() {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const currentDate = new Date();
-  const currentDay = daysOfWeek[currentDate.getUTCDay()];
-  const currentUTC = currentDate.getTime();
-  document.getElementById("currentDay").textContent = currentDay;
-  document.getElementById("currentUTC").textContent = currentUTC;
-}
-updateDateTime();
-setInterval(updateDateTime, 1000);
+const labelDay = document.querySelector(".day");
+const labelTime = document.querySelector(".time");
+
+const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const getTodayDate = (_) => {
+  return new Date();
+};
+
+const getDay = (_) => {
+  return getTodayDate().getUTCDay();
+};
+
+labelDay.textContent = weekDays[getDay()];
+setInterval(() => {
+  labelTime.textContent = new Date().getTime();
+}, 1000)
